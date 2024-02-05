2dec6493ea2fb7933e73d884b83f98943d243802
yuniroro <ilho7159@gmail.com>
2024-02-05 15:41:20 +0900

    initial commit

---
+++ dataloader.py
@@ -0,0 +1,185 @@
+# python native
+import os
+import json
+import random
+import datetime
+from functools import partial
+
+# external library
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+from sklearn.model_selection import GroupKFold
+import albumentations as A
+
+# torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+# visualization
+import matplotlib.pyplot as plt
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+
+class XRayDataset(Dataset):
+    def __init__(self, IMAGE_ROOT, LABEL_ROOT, is_train=True, transforms=None):
+        pngs = {
+            os.path.relpath(os.path.join(root, fname), start=IMAGE_ROOT)
+            for root, _dirs, files in os.walk(IMAGE_ROOT)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".png"
+        }
+
+        jsons = {
+            os.path.relpath(os.path.join(root, fname), start=LABEL_ROOT)
+            for root, _dirs, files in os.walk(LABEL_ROOT)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".json"
+        }
+
+        pngs = sorted(pngs)
+        jsons = sorted(jsons)
+        
+        _filenames = np.array(pngs)
+        _labelnames = np.array(jsons)
+        
+        # split train-valid
+        # 한 폴더 안에 한 인물의 양손에 대한 `.dcm` 파일이 존재하기 때문에
+        # 폴더 이름을 그룹으로 해서 GroupKFold를 수행합니다.
+        # 동일 인물의 손이 train, valid에 따로 들어가는 것을 방지합니다.
+        groups = [os.path.dirname(fname) for fname in _filenames]
+        
+        # dummy label
+        ys = [0 for fname in _filenames]
+        
+        # 전체 데이터의 20%를 validation data로 쓰기 위해 `n_splits`를
+        # 5으로 설정하여 KFold를 수행합니다.
+        gkf = GroupKFold(n_splits=5)
+        
+        filenames = []
+        labelnames = []
+        for i, (x, y) in enumerate(gkf.split(_filenames, ys, groups)):
+            if is_train:
+                # 0번을 validation dataset으로 사용합니다.
+                if i == 0:
+                    continue
+                    
+                filenames += list(_filenames[y])
+                labelnames += list(_labelnames[y])
+            
+            else:
+                filenames = list(_filenames[y])
+                labelnames = list(_labelnames[y])
+                
+                # skip i > 0
+                break
+        
+        self.IMAGE_ROOT = IMAGE_ROOT
+        self.LABEL_ROOT = LABEL_ROOT
+        self.filenames = filenames
+        self.labelnames = labelnames
+        self.is_train = is_train
+        self.transforms = transforms
+        
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, item):
+        image_name = self.filenames[item]
+        image_path = os.path.join(self.IMAGE_ROOT, image_name)
+        
+        image = cv2.imread(image_path)
+        image = image / 255.
+        
+        label_name = self.labelnames[item]
+        label_path = os.path.join(self.LABEL_ROOT, label_name)
+        
+        # (H, W, NC) 모양의 label을 생성합니다.
+        label_shape = tuple(image.shape[:2]) + (len(CLASSES), )
+        label = np.zeros(label_shape, dtype=np.uint8)
+        
+        # label 파일을 읽습니다.
+        with open(label_path, "r") as f:
+            annotations = json.load(f)
+        annotations = annotations["annotations"]
+        
+        # 클래스 별로 처리합니다.
+        for ann in annotations:
+            c = ann["label"]
+            class_ind = CLASS2IND[c]
+            points = np.array(ann["points"])
+            
+            # polygon 포맷을 dense한 mask 포맷으로 바꿉니다.
+            class_label = np.zeros(image.shape[:2], dtype=np.uint8)
+            cv2.fillPoly(class_label, [points], 1)
+            label[..., class_ind] = class_label
+        
+        if self.transforms is not None:
+            inputs = {"image": image, "mask": label} if self.is_train else {"image": image}
+            result = self.transforms(**inputs)
+            
+            image = result["image"]
+            label = result["mask"] if self.is_train else label
+
+        # to tenser will be done later
+        image = image.transpose(2, 0, 1)    # channel first 포맷으로 변경합니다.
+        label = label.transpose(2, 0, 1)
+        
+        image = torch.from_numpy(image).float()
+        label = torch.from_numpy(label).float()
+            
+        return image, label
+
+
+class XRayInferenceDataset(Dataset):
+    def __init__(self, IMAGE_ROOT, transforms=None):
+        pngs = {
+            os.path.relpath(os.path.join(root, fname), start=IMAGE_ROOT)
+            for root, _dirs, files in os.walk(IMAGE_ROOT)
+            for fname in files
+            if os.path.splitext(fname)[1].lower() == ".png"
+        }
+            
+        _filenames = pngs
+        _filenames = np.array(sorted(_filenames))
+        
+        self.IMAGE_ROOT = IMAGE_ROOT
+        self.filenames = _filenames
+        self.transforms = transforms
+    
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, item):
+        image_name = self.filenames[item]
+        image_path = os.path.join(self.IMAGE_ROOT, image_name)
+        
+        image = cv2.imread(image_path)
+        image = image / 255.
+        
+        if self.transforms is not None:
+            inputs = {"image": image}
+            result = self.transforms(**inputs)
+            image = result["image"]
+
+        # to tenser will be done later
+        image = image.transpose(2, 0, 1)  
+        
+        image = torch.from_numpy(image).float()
+            
+        return image, image_name
---
+++ train.py
@@ -0,0 +1,219 @@
+# python native
+import os
+import json
+import random
+import datetime
+from functools import partial
+
+# external library
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+from sklearn.model_selection import GroupKFold
+import albumentations as A
+import yaml
+import wandb
+
+# torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+# visualization
+import matplotlib.pyplot as plt
+from dataloader import XRayDataset
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = y_true.flatten(2)
+    y_pred_f = y_pred.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+    
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
+
+
+def save_model(model, file_name='model.pt'):
+    output_path = os.path.join(SAVED_DIR, file_name)
+    torch.save(model, output_path)
+
+
+def set_seed():
+    torch.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed_all(RANDOM_SEED) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(RANDOM_SEED)
+    random.seed(RANDOM_SEED)
+
+
+def validation(epoch, model, data_loader, criterion, thr=0.5):
+    print(f'Start validation #{epoch:2d}')
+    model.eval()
+
+    dices = []
+    with torch.no_grad():
+        n_class = len(CLASSES)
+        total_loss = 0
+        cnt = 0
+
+        for step, (images, masks) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            images, masks = images.cuda(), masks.cuda()         
+            model = model.cuda()
+            
+            outputs = model(images)['out']
+            
+            output_h, output_w = outputs.size(-2), outputs.size(-1)
+            mask_h, mask_w = masks.size(-2), masks.size(-1)
+            
+            # gt와 prediction의 크기가 다른 경우 prediction을 gt에 맞춰 interpolation 합니다.
+            if output_h != mask_h or output_w != mask_w:
+                outputs = F.interpolate(outputs, size=(mask_h, mask_w), mode="bilinear")
+            
+            loss = criterion(outputs, masks)
+            total_loss += loss
+            cnt += 1
+            
+            outputs = torch.sigmoid(outputs)
+            outputs = (outputs > thr).detach().cpu()
+            masks = masks.detach().cpu()
+            
+            dice = dice_coef(outputs, masks)
+            dices.append(dice)
+                
+    dices = torch.cat(dices, 0)
+    dices_per_class = torch.mean(dices, 0)
+    dice_str = [
+        f"{c:<12}: {d.item():.4f}"
+        for c, d in zip(CLASSES, dices_per_class)
+    ]
+    dice_str = "\n".join(dice_str)
+    print(dice_str)
+    
+    avg_dice = torch.mean(dices_per_class).item()
+    wandb.log({"val/loss": total_loss / cnt, "val/dice": avg_dice}, step=epoch)
+    for c, d in zip(CLASSES, dices_per_class):
+        wandb.log({f"val-class/dice_{c}": d.item()}, step=epoch)
+    return avg_dice
+
+
+def train(model, data_loader, val_loader, criterion, optimizer):
+    print(f'Start training..')
+    
+    n_class = len(CLASSES)
+    best_dice = 0.
+    
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+
+        for step, (images, masks) in enumerate(data_loader):            
+            # gpu 연산을 위해 device 할당합니다.
+            images, masks = images.cuda(), masks.cuda()
+            model = model.cuda()
+            
+            outputs = model(images)['out']
+            
+            # loss를 계산합니다.
+            loss = criterion(outputs, masks)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # step 주기에 따라 loss를 출력합니다.
+            if (step + 1) % 10 == 0:
+                print(
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | '
+                    f'Epoch [{epoch+1}/{NUM_EPOCHS}], '
+                    f'Step [{step+1}/{len(train_loader)}], '
+                    f'Loss: {round(loss.item(),4)}'
+                )
+        
+        wandb.log({"train/loss": loss.item()}, step=epoch)
+
+        # validation 주기에 따라 loss를 출력하고 best model을 저장합니다.
+        if (epoch + 1) % VAL_EVERY == 0:
+            dice = validation(epoch + 1, model, val_loader, criterion)
+            
+            if best_dice < dice:
+                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {dice:.4f}")
+                print(f"Save model in {SAVED_DIR}")
+                best_dice = dice
+                save_model(model)
+        
+        torch.cuda.empty_cache()
+
+
+if __name__ == '__main__':
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    DATA_ROOT = config['DATA_ROOT']
+    IMAGE_ROOT = f"{DATA_ROOT}/train/DCM"
+    LABEL_ROOT = f"{DATA_ROOT}/train/outputs_json"
+    SAVED_DIR = config['SAVED_DIR']
+
+    if not os.path.exists(SAVED_DIR):                                                           
+        os.makedirs(SAVED_DIR)
+
+    BATCH_SIZE = config['BATCH_SIZE']
+    LR = config['LR']
+    RANDOM_SEED = config['RANDOM_SEED']
+    NUM_EPOCHS = config['NUM_EPOCHS']
+    VAL_EVERY = config['VAL_EVERY']
+
+    tf = A.Resize(512, 512)
+    train_dataset = XRayDataset(IMAGE_ROOT, LABEL_ROOT, is_train=True, transforms=tf)
+    valid_dataset = XRayDataset(IMAGE_ROOT, LABEL_ROOT, is_train=False, transforms=tf)
+
+    train_loader = DataLoader(
+        dataset=train_dataset, 
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=8,
+        drop_last=True,
+    )
+
+    # 주의: validation data는 이미지 크기가 크기 때문에 `num_wokers`는 커지면 메모리 에러가 발생할 수 있습니다.
+    valid_loader = DataLoader(
+        dataset=valid_dataset, 
+        batch_size=8,
+        shuffle=False,
+        num_workers=0,
+        drop_last=False
+    )
+
+    model = models.segmentation.fcn_resnet50(pretrained=True)
+
+    # output class 개수를 dataset에 맞도록 수정합니다.
+    model.classifier[4] = nn.Conv2d(512, len(CLASSES), kernel_size=1)
+
+    # Loss function을 정의합니다.
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Optimizer를 정의합니다.
+    optimizer = optim.Adam(params=model.parameters(), lr=LR, weight_decay=1e-6)
+
+    # 시드를 설정합니다.
+    set_seed()
+
+    CAMPER_ID = config['CAMPER_ID']
+    EXP_NAME = config['EXP_NAME']
+    wandb.init(project='Boost Camp Lv2-3', name=f"{CAMPER_ID}-{EXP_NAME}", config=config)
+    
+    train(model, train_loader, valid_loader, criterion, optimizer)
