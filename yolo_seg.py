4acb1d53c14a76fec5b6b70ad4c965aad77c305a
patrashu <qhdrmfdl123@syuin.ac.kr>
2024-02-10 08:57:34 +0900

    feat: modified yolo_config.yaml to customize yolo_seg training
    #39

--- yolo_seg.py
+++ yolo_seg.py
@@ -1,5 +1,8 @@
+import cv2
 import yaml
 import wandb
+from glob import glob
+from tqdm import tqdm
 from ultralytics import YOLO
 
 
@@ -16,6 +19,10 @@ def train(config_path: str):
     
     model = YOLO("yolov8x-seg.pt")
     train_option = config["train_option"]
+    print(train_option["close_mosaic"])
+    
+    # want to customize, See under page
+    # https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/default.yaml
     model.train(
         data=config_path,
         epochs=train_option["epochs"],
@@ -23,8 +30,34 @@ def train(config_path: str):
         device=train_option["device"],
         batch=train_option["batch"],
         workers=train_option["workers"],
+        close_mosaic=train_option["close_mosaic"],
+        cos_lr=train_option["cos_lr"],
+        optimizer=train_option["optimizer"],
     )
+
+def inference():
+    model = YOLO(f"runs/segment/train/weights/best.pt").cuda()
+    infer_images = sorted(glob("data/test/*/*/*.png"))
     
+    rles = []
+    filename_and_class = []
+    for idx, infer_image in tqdm(enumerate(infer_images)):
+        result = model.predict(infer_image, imgsz=2048, conf=0.5)[0]
+        res = result.plot()
+        cv2.imwrite(f"result_{idx}.png", res)   
+        
+        boxes = result.boxes
+        masks = result.masks
+        print(boxes, masks)
+        break
+        
+        
+        
+        
+        
+    pass
+
     
 if __name__ == '__main__':
-    train('yolo_config.yaml')
+    # train('yolo_config.yaml')
+    inference()
